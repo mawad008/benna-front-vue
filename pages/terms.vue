78f1ec86a28cc606064adb0a09874fd2d5908e37
marwam <marwamomeno98@gmail.com>
2025-04-07 13:21:35 +0200

    modify terms page style

--- pages/terms.vue
+++ pages/terms.vue
@@ -1,6 +1,6 @@
 <template>
   <Hero />
-  <div class="lg:mt-10 md:mt-20 container">
+  <div class="lg:mt-10 md:mt-20 mt-10 container">
     <h1 class="text-2xl font-bold mb-4 text-primary">الشروط والأحكام</h1>
 
     <div class="space-y-4">
