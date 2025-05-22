bd3482809e5b9edf1fe66cefdb155eed1501574c
marwam <marwamomeno98@gmail.com>
2025-05-22 16:28:32 +0300

    add footer referral

--- components/layout/LayoutFooter.vue
+++ components/layout/LayoutFooter.vue
@@ -1,11 +1,28 @@
 <template>
   <footer class="bg-white text-dark py-4 border-t border-gray-300 h-[6vh]">
-    <div class="container mx-auto flex flex-col md:flex-row justify-between items-center text-sm px-4 py-2 gap-2">
+    <div
+      class="container mx-auto flex flex-col md:flex-row justify-between items-center text-sm px-4 py-2 gap-2"
+    >
       <p>جمعية بناء لرعاية الأيتام برخصة 568</p>
 
-      <p class="flex items-center gap-3 ">
+      <p class="flex items-center gap-3">
         تم التطوير بواسطة
-        <img src="/WS-logo.png" alt="Webstdy Logo" class="h-5" />
+        <!-- <img src="/WS-logo.png" alt="Webstdy Logo" class="h-5" /> -->
+
+        <a
+          title="شركة ويب ستدي لخدمات تصميم و برمجة المواقع و التطبيقات"
+          href="https://webstdy.com/ar?utm_source=benna-foorter&amp;utm_medium=referral"
+          target="_blank"
+          rel="noopener"
+        >
+          <img
+            class="lazyloaded h-5"
+            title="شركة ويب ستدي لخدمات تصميم و برمجة المواقع و التطبيقات"
+            src="/WS-logo.png"
+            alt="شركة ويب ستدي لخدمات تصميم و برمجة المواقع و التطبيقات"
+            data-src="https://webstdy.com/CDN/cr.png"
+          />
+        </a>
       </p>
 
       <div class="flex items-center gap-2">
@@ -17,6 +34,4 @@
   </footer>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
